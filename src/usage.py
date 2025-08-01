685fd22b83c398099337591ac7015a5cd6014451
basaran3mir <ebasaran999@gmail.com>
2025-08-01 10:11:41 +0300

    v1.1

--- src/usage.py
+++ src/usage.py
@@ -1,8 +1,5 @@
 from qr_code_generator import QRCodeGenerator
 
-def run(data, output_flie):
-    generator = QRCodeGenerator(data, output_flie)
-    generator.generate()
-
 if __name__ == "__main__":
-    run("https://basaran3mir.github.io/", "src/output/qr_code.png")
+    generator = QRCodeGenerator("https://basaran3mir.github.io/", "output/qr_code.png")
+    generator.generate()
