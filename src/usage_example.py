5c408d037123bdc7ca6218b8a1128390964c60aa
basaran3mir <ebasaran999@gmail.com>
2026-06-11 11:28:09 +0300

    Refactor code structure for improved readability and maintainability

--- src/usage_example.py
+++ src/usage_example.py
@@ -1,6 +1,6 @@
 from qr_code_generator import QRCodeGenerator
 
 if __name__ == "__main__":
-    url = "https://basaran3mir.github.io/"
+    url = "https://emirbasaran.com"
     generator = QRCodeGenerator(url, "output/qr_code.png")
     generator.generate()
